ae16a9a48dc6379a8d9b04d676f78075f641cf5a
Diego Teran <dteran.sch@gmail.com>
2025-10-21 13:48:09 -0500

    Remodified file structure, app.py, and App.js

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from markupsafe import escape
 
 app = Flask(__name__)
 # Allow requests from the React dev server (and others) during development
 CORS(app)
 
 
-@app.route("/hello")
+@app.route("/api/hello") # Use the '/api/...' prefix for all backend routes to make it clear they're API endpoints
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify(message="Hello, World!") #Return JSON instead of HTML. It's easier for React to consume
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message || "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
